2e3b0d5080edd59d9889859a800a891006eecff9
dhruvv-raghu <raghudhruv2@gmail.com>
2025-04-08 15:46:19 +0530

    fixes

--- apps/lbxd/app/page.tsx
+++ apps/lbxd/app/page.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-   <div className="p-5 mx-2 font-main my-4 text-2xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nam tempora. Sequi corrupti animi eius, earum velit, officiis id tempore at labore ut cupiditate dicta. Magnam est alias illum magni!  </div>
+<div className= "font-main"> 
+  <div className="p-5 mx-2 my-4 text-8xl">
+       Entertainment
+       <div className="p-5 mx-2 my-4 text-2xl">is never an objective truth</div>
+  </div>
+  <br />
+  <div className="p-5 mx-2 my-4 text-2xl">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nam tempora. Sequi corrupti animi eius, earum velit, officiis id tempore at labore ut cupiditate dicta. Magnam est alias illum magni!  
+  </div>
+</div> 
   );
 }
